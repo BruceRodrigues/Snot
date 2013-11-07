e809d62223b36be022e878f2a43e3dce365f480b
Bruce <brucerodrigues@githutb.com>
2013-11-07 11:48:22 -0200

    Added Symbol table

--- src/MySnotListener.java
+++ src/MySnotListener.java
@@ -1,17 +1,25 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.tree.TerminalNode;
 import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class MySnotListener extends SnotBaseListener {
+	SymbolTable sym = new SymbolTable();
+	
 	//Lista de tipos definidos
 	public ArrayList<String> types_list = new ArrayList<String>();
 
 	//Lista de IDs declarados
 	public ArrayList<String> id_list = new ArrayList<String>();
-
-	@Override public void enterExpression(SnotParser.ExpressionContext ctx) { }
+	
+	public ArrayList<Variavel> vars = new ArrayList<Variavel>();
+	
+	@Override public void enterExpression(SnotParser.ExpressionContext ctx) {
+		
+	}
 	@Override public void exitExpression(SnotParser.ExpressionContext ctx) { }
 
 	@Override public void enterArgument_list(SnotParser.Argument_listContext ctx) { }
@@ -22,12 +30,12 @@ public class MySnotListener extends SnotBaseListener {
 
 	@Override public void enterFunction_declaration(SnotParser.Function_declarationContext ctx) {
 		//Verificar tipo de retorno
-		if(ctx.type().ID() != null && !types_list.contains(ctx.type().ID().toString())) {
+		if(ctx.type().ID() != null && !sym.containsType(ctx.type().ID().toString())) {
 			System.err.println("Erro-> Tipo de retorno nao conhecido: " + ctx.type().ID());	
 		}
 		
 		//Verificar se nome ta funcao ja eh usado em algum lugar
-		if(id_list.contains(ctx.ID().getText()))
+		if(sym.containsFunction(ctx.ID().getText()))
 			System.err.println("Erro-> Id ja declarado: " + ctx.ID().getText());
 
 		//Verificar tipo dos parametros
@@ -35,14 +43,14 @@ public class MySnotListener extends SnotBaseListener {
 			List<SnotParser.TypeContext> t = ctx.parameter_list().type();
 			List<TerminalNode> id = ctx.parameter_list().ID();
 			for(int i = 0; i < t.size(); i++) {
-				if(t.get(i).ID() != null && !types_list.contains(t.get(i).getText()))
+				if(t.get(i).ID() != null && !sym.containsType(t.get(i).getText()))
 					System.err.println("Erro-> Tipo nao reconhecido: " + t.get(i).getText());
 					
 			}	
 		}	
 
 		//Adiciona a funcao na lista de IDs, assim ela pode ser chamada
-		id_list.add(ctx.ID().toString());
+		sym.addFunction(ctx.ID().toString());
 
 	}
 	@Override public void exitFunction_declaration(SnotParser.Function_declarationContext ctx) { }
@@ -52,10 +60,10 @@ public class MySnotListener extends SnotBaseListener {
 
 	@Override public void enterClass_declaration(SnotParser.Class_declarationContext ctx) { 
 		//Adicionando ID da classe na lista de tipos possiveis
-		if(types_list.contains(ctx.ID().toString()))
+		if(sym.containsClass(ctx.ID().toString()))
 			System.out.println("Erro-> Classe ja definida: " + ctx.ID());		
 		else
-			types_list.add(ctx.ID().toString()); 
+			sym.addClass(ctx.ID().toString()); 
 
 	}
 	@Override public void exitClass_declaration(SnotParser.Class_declarationContext ctx) { 
@@ -70,7 +78,7 @@ public class MySnotListener extends SnotBaseListener {
 		if(ctx.argument_list() != null) {
 			List<SnotParser.ArgumentContext> list = ctx.argument_list().argument();
 			for(int i = 0; i < list.size(); i++) {
-				if(list.get(i).ID() != null && !id_list.contains(list.get(i).getText()))
+				if(list.get(i).ID() != null && !sym.contains(list.get(i).getText()))
 					System.err.println("Erro-> argumento nao declarado: " + list.get(i).getText());
 			}	
 		}
@@ -89,6 +97,7 @@ public class MySnotListener extends SnotBaseListener {
 	@Override public void enterProgram(SnotParser.ProgramContext ctx) { }
 	@Override public void exitProgram(SnotParser.ProgramContext ctx) { }
 
+	//Nao precisa implementar
 	@Override public void enterLoop(SnotParser.LoopContext ctx) { }
 	@Override public void exitLoop(SnotParser.LoopContext ctx) { }
 
@@ -103,26 +112,28 @@ public class MySnotListener extends SnotBaseListener {
 
 	@Override public void enterVar_declaration(SnotParser.Var_declarationContext ctx) { 
 		//Verificar tipo da variavel
-		if(ctx.type().ID() != null && !types_list.contains(ctx.type().ID().toString())) {
+		if(ctx.type().ID() != null && !sym.containsType(ctx.type().ID().toString())) {
 			System.err.println("Erro-> Tipo nao conhecido: " + ctx.type().ID());	
 		}
 		
 		//Verificar se nome da var ja eh usado em algum lugar
-		if(id_list.contains(ctx.ID().getText()))
+		if(sym.contains(ctx.ID().getText()))
 			System.err.println("Erro-> Id ja declarado: " + ctx.ID().getText());
 
 		
 		//Adiciona a funcao na lista de IDs, assim ela pode ser chamada
-		id_list.add(ctx.ID().toString());
+		sym.addFunction(ctx.ID().toString());
 	}
 	@Override public void exitVar_declaration(SnotParser.Var_declarationContext ctx) { }
 
 	@Override public void enterParameter_list(SnotParser.Parameter_listContext ctx) { 
-		if (idList.contains(ctx.ID().getText())) {
-			//Error, this id is alredy used
-		}
-		if (!idList.constais(ctx.ID().getText() && !isPreDefinedType(ctx.type().getText())) {
-			//Error, this type was not defined
+		for (TypeContext typeCtx : ctx.type()) {
+			if (types_list.contains(typeCtx.ID().getText())) {
+				System.out.println("Error, "+typeCtx.ID().getText()+" is alredy used");
+			}
+			if (!types_list.constais(typeCtx.ID().getText()) && !isPreDefinedType(typeCtx.type().getText())) {
+				System.out.println("Error, "+typeCtx.type().getText()+" was not defined");
+			}
 		}
 	}
 	@Override public void exitParameter_list(SnotParser.Parameter_listContext ctx) { }
