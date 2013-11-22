9fc68a45aaad874e733553f627144d8ab4ef7435
Bruce <brucerodrigues@githutb.com>
2013-11-22 10:21:34 -0200

    .

--- src/MySnotListener.java
+++ src/MySnotListener.java
@@ -17,12 +17,6 @@ public class MySnotListener extends SnotBaseListener {
 			printError(ctx, "Variavel nao declarada: " + ctx.ID().getText());
 	    }
 	}
-	
-	@Override public void enterArgument_list(SnotParser.Argument_listContext ctx) { }
-	@Override public void exitArgument_list(SnotParser.Argument_listContext ctx) { }
-
-	@Override public void enterArgument(SnotParser.ArgumentContext ctx) { }
-	@Override public void exitArgument(SnotParser.ArgumentContext ctx) { }
 
 	@Override public void enterFunction_declaration(SnotParser.Function_declarationContext ctx) {
 		//Verificar tipo de retorno
@@ -41,18 +35,11 @@ public class MySnotListener extends SnotBaseListener {
 			for(int i = 0; i < t.size(); i++) {
 				if(t.get(i).ID() != null && !sym.containsType(t.get(i).getText()))
 					System.err.println("Erro-> Tipo nao reconhecido: " + t.get(i).getText());
-					
 			}	
 		}	
-
 		//Adiciona a funcao na lista de IDs, assim ela pode ser chamada
 		sym.addFunction(ctx.ID().toString());
-
 	}
-	@Override public void exitFunction_declaration(SnotParser.Function_declarationContext ctx) { }
-
-	@Override public void enterDeclaration(SnotParser.DeclarationContext ctx) { }
-	@Override public void exitDeclaration(SnotParser.DeclarationContext ctx) { }
 
 	@Override public void enterClass_declaration(SnotParser.Class_declarationContext ctx) { 
 		//Adicionando ID da classe na lista de tipos possiveis
@@ -61,8 +48,6 @@ public class MySnotListener extends SnotBaseListener {
 		else
 			sym.addClass(ctx.ID().toString()); 
 
-	}
-	@Override public void exitClass_declaration(SnotParser.Class_declarationContext ctx) { 
 	}
 
 	@Override public void enterCall_procedure(SnotParser.Call_procedureContext ctx) { 
@@ -79,29 +64,6 @@ public class MySnotListener extends SnotBaseListener {
 			}	
 		}
 	}
-	@Override public void exitCall_procedure(SnotParser.Call_procedureContext ctx) { }
-
-	@Override public void enterType(SnotParser.TypeContext ctx) { }
-	@Override public void exitType(SnotParser.TypeContext ctx) { }
-
-	@Override public void enterSelection(SnotParser.SelectionContext ctx) { }
-	@Override public void exitSelection(SnotParser.SelectionContext ctx) { }
-
-	@Override public void enterCall_method(SnotParser.Call_methodContext ctx) { }
-	@Override public void exitCall_method(SnotParser.Call_methodContext ctx) { }
-
-	@Override public void enterProgram(SnotParser.ProgramContext ctx) { }
-	@Override public void exitProgram(SnotParser.ProgramContext ctx) { }
-
-	//Nao precisa implementar
-	@Override public void enterLoop(SnotParser.LoopContext ctx) { }
-	@Override public void exitLoop(SnotParser.LoopContext ctx) { }
-
-	@Override public void enterValue(SnotParser.ValueContext ctx) { }
-	@Override public void exitValue(SnotParser.ValueContext ctx) { }
-
-	@Override public void enterCommand(SnotParser.CommandContext ctx) { }
-	@Override public void exitCommand(SnotParser.CommandContext ctx) { }
 
 	@Override public void enterAttribution(SnotParser.AttributionContext ctx) { 
 		//Verificar se nome da var nao eh existente
@@ -118,8 +80,6 @@ public class MySnotListener extends SnotBaseListener {
 		} 
 	}
 
-	@Override public void exitAttribution(SnotParser.AttributionContext ctx) { }
-
 	@Override public void enterVar_declaration(SnotParser.Var_declarationContext ctx) { 
 		//Verificar tipo da variavel
 		if(ctx.type().ID() != null && !sym.containsType(ctx.type().ID().toString())) {
@@ -134,16 +94,6 @@ public class MySnotListener extends SnotBaseListener {
 		printDebug("Declarando variavel " + ctx.ID().getText());
 		sym.addFunction(ctx.ID().toString());
 	}
-	@Override public void exitVar_declaration(SnotParser.Var_declarationContext ctx) { }
-
-	@Override public void enterParameter_list(SnotParser.Parameter_listContext ctx) { 
-	}
-	@Override public void exitParameter_list(SnotParser.Parameter_listContext ctx) { }
-
-	@Override public void enterEveryRule(ParserRuleContext ctx) { }
-	@Override public void exitEveryRule(ParserRuleContext ctx) { }
-	@Override public void visitTerminal(TerminalNode node) { }
-	@Override public void visitErrorNode(ErrorNode node) { }
 
 	private void printError(ParserRuleContext ctx, String erro) {
 		System.err.println("Erro:"+ctx.getStart().getLine()+":-> "+ erro);
